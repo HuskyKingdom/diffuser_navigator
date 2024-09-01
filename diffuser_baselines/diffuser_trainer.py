4611df1ef5e02cc36ed1cca7c5426ed503382a2a
Yuhang Song <1595025647@qq.com>
2024-09-01 15:16:59 +0800

    dataloader exp.

--- diffuser_baselines/diffuser_trainer.py
+++ diffuser_baselines/diffuser_trainer.py
@@ -521,6 +521,7 @@ class DiffuserTrainer(BaseVLNCETrainer):
                     self._update_dataset(
                         dagger_it + (1 if self.config.IL.load_from_ckpt else 0)
                     )
+                    assert 1==2
                 
                 assert 1==2
                 # get dataset ---
