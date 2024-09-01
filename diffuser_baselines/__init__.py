9f3ca49fcd56be25a8070570f7dcbfb5d1cb966b
Yuhang Song <1595025647@qq.com>
2024-09-01 12:19:48 +0800

    dataloader exp.

--- diffuser_baselines/__init__.py
+++ diffuser_baselines/__init__.py
@@ -1,7 +1,5 @@
-from vlnce_baselines import (
-    dagger_trainer,
-    ddppo_waypoint_trainer,
-    recollect_trainer,
+from diffuser_baselines import (
+    diffuser_trainer
 )
 from vlnce_baselines.common import environments
 from vlnce_baselines.models import cma_policy, seq2seq_policy, waypoint_policy
