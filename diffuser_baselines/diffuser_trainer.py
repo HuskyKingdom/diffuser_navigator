235142e014635f711ae0730b23375011fa213202
Yuhang Song <1595025647@qq.com>
2024-09-01 15:16:15 +0800

    dataloader exp.

--- diffuser_baselines/diffuser_trainer.py
+++ diffuser_baselines/diffuser_trainer.py
@@ -506,15 +506,7 @@ class DiffuserTrainer(BaseVLNCETrainer):
             action_space=action_space,
         )
 
-        # get dataset ---
-        step_id = 0
-        if not self.config.IL.DAGGER.preload_lmdb_features:
-            self._update_dataset(
-                dagger_it + (1 if self.config.IL.load_from_ckpt else 0)
-            )
-        
-        assert 1==2
-        # get dataset ---
+
 
         with TensorboardWriter(
             self.config.TENSORBOARD_DIR,
@@ -522,6 +514,16 @@ class DiffuserTrainer(BaseVLNCETrainer):
             purge_step=0,
         ) as writer:
             for dagger_it in range(self.config.IL.DAGGER.iterations):
+
+                # get dataset ---
+                step_id = 0
+                if not self.config.IL.DAGGER.preload_lmdb_features:
+                    self._update_dataset(
+                        dagger_it + (1 if self.config.IL.load_from_ckpt else 0)
+                    )
+                
+                assert 1==2
+                # get dataset ---
                     
                 if torch.cuda.is_available():
                     with torch.cuda.device(self.device):
