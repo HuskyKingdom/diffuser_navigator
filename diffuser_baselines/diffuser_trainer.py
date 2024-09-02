720721081e599a4315f53b601b744af5d1fa39b4
Yuhang Song <1595025647@qq.com>
2024-09-02 16:19:24 +0800

    dataloader exp.

--- diffuser_baselines/diffuser_trainer.py
+++ diffuser_baselines/diffuser_trainer.py
@@ -162,10 +162,10 @@ class TrajectoryDataset(torch.utils.data.Dataset):
                 
                 trajectory = msgpack_numpy.unpackb(data, raw=False)
 
-                print(f"instruction {trajectory[0]["instruction"].shape}")
-                print(f"p {trajectory[0]["progress"].shape}")
-                print(f"r {trajectory[0]["rgb_feature"].shape}")
-                print(f"d {trajectory[0]["depth_feature"].shape}")
+                print(f"""instruction {trajectory[0]["instruction"].shape}""")
+                print(f"""p {trajectory[0]["progress"].shape}""")
+                print(f"""r {trajectory[0]["rgb_feature"].shape}""")
+                print(f"""d {trajectory[0]["depth_feature"].shape}""")
                 return trajectory
 
 
