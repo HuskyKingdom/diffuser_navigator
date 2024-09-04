688b44362913064a51cec52d9cce69b9a1bb6e5c
Yuhang Song <1595025647@qq.com>
2024-09-04 13:39:08 +0800

    dataloader exp.

--- diffuser_baselines/__init__.py
+++ diffuser_baselines/__init__.py
@@ -2,4 +2,4 @@ from diffuser_baselines import (
     diffuser_trainer
 )
 from vlnce_baselines.common import environments
-from vlnce_baselines.models import cma_policy, seq2seq_policy, waypoint_policy
+from diffuser_baselines.models import diffuser_navigator
