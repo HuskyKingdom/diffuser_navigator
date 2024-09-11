694262e2a4f408320ad235ac0f2b375843d162ce
Yuhang Song <1595025647@qq.com>
2024-09-11 14:39:21 +0800

    baseline overfitting exp.

--- diffuser_baselines/models/diffuser_navigator.py
+++ diffuser_baselines/models/diffuser_navigator.py
@@ -182,7 +182,7 @@ class DiffusionNavigator(nn.Module):
         noise = torch.randn(oracle_action_tokens.shape, device=oracle_action_tokens.device)
 
         noising_timesteps = torch.randint(
-            900,
+            49,
             self.noise_scheduler.config.num_train_timesteps,
             (len(noise),), device=noise.device
         ).long()
@@ -206,7 +206,7 @@ class DiffusionNavigator(nn.Module):
 
         # print(f"shapes : {pred.shape} ; {noising_timesteps.shape} ; {noised_orc_action_tokens.shape}")
         step_out = self.noise_scheduler.step(
-            pred[0].unsqueeze(0), noising_timesteps[0], noised_orc_action_tokens[0].unsqueeze(0)
+            pred[30].unsqueeze(0), noising_timesteps[30], noised_orc_action_tokens[0].unsqueeze(0)
         )
 
         denoised = step_out["prev_sample"]
