d9ca3745b80e370e9859622e59a8c6f6bb7cb56c
Yuhang Song <1595025647@qq.com>
2024-09-12 15:43:33 +0800

    baseline overfitting exp.

--- diffuser_baselines/models/diffuser_navigator.py
+++ diffuser_baselines/models/diffuser_navigator.py
@@ -207,26 +207,26 @@ class DiffusionNavigator(nn.Module):
         print(f"GroundTruth Actions {observations['gt_actions'][0]}")
         # print(f"shapes : {pred.shape} ; {noising_timesteps.shape} ; {noised_orc_action_tokens.shape}")
         step_out = self.noise_scheduler.step(
-            pred[0].unsqueeze(0), noising_timesteps[0], noised_orc_action_tokens[0].unsqueeze(0)
+            pred[30].unsqueeze(0), noising_timesteps[30], noised_orc_action_tokens[0].unsqueeze(0)
         )
         denoised = step_out["prev_sample"]
         pre_actions = self.retrive_action_from_em(denoised)
         print(f"Predicted Actions {pre_actions}")
 
 
-        # analyzing
-        list1 = pre_actions.squeeze(0).cpu().tolist()
-        list2 = observations['gt_actions'][0].cpu().tolist()
+        # # analyzing
+        # list1 = pre_actions.squeeze(0).cpu().tolist()
+        # list2 = observations['gt_actions'][0].cpu().tolist()
 
-        same_index_count = sum(1 for a, b in zip(list1, list2) if a == b)
-        self.total_correct += same_index_count
+        # same_index_count = sum(1 for a, b in zip(list1, list2) if a == b)
+        # self.total_correct += same_index_count
 
-        if self.total_evaled < 100:
-            self.total_evaled += 5
-        else:
-            print(self.total_correct)
-            print(f"evaluated {self.total_evaled} | accuracy {self.total_correct / (self.total_evaled)}")
-            assert 1==2
+        # if self.total_evaled < 100:
+        #     self.total_evaled += 5
+        # else:
+        #     print(self.total_correct)
+        #     print(f"evaluated {self.total_evaled} | accuracy {self.total_correct / (self.total_evaled)}")
+        #     assert 1==2
 
 
         # compute loss
