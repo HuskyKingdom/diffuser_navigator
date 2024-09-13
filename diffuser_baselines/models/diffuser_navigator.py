0d2f2cfb989bf5ed54aa5f60dac1e331e555a612
Yuhang Song <1595025647@qq.com>
2024-09-13 15:40:25 +0800

    underfitting exp.

--- diffuser_baselines/models/diffuser_navigator.py
+++ diffuser_baselines/models/diffuser_navigator.py
@@ -220,7 +220,7 @@ class DiffusionNavigator(nn.Module):
 
             
             step_out = self.noise_scheduler.step(
-                pred_noises, t, intermidiate_noise
+                pred_noises, random.randint(0,1000), intermidiate_noise
             )
 
             intermidiate_noise = step_out["prev_sample"]
