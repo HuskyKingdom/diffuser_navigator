f2d3bb59d6093c62b0ed500c767113458f6eb5a6
Yuhang Song <1595025647@qq.com>
2024-09-17 15:40:29 +0800

    action noising first exp. | testing

--- diffuser_baselines/models/diffuser_navigator.py
+++ diffuser_baselines/models/diffuser_navigator.py
@@ -279,6 +279,24 @@ class DiffusionNavigator(nn.Module):
 
         print(f"original onehot {encoded_actions[0]}")
         print(f"noised onehot {noised_one_hot[0]} | {F.softmax(noised_one_hot[0],dim=-1)} | {noised_one_hot[0].shape}")
+
+        denoise_steps = list(range(999, -1, -1))
+        intermidiate_noise = noised_one_hot
+        for t in denoise_steps: 
+
+            # noise pred.
+            with torch.no_grad():
+                
+                pred_noises = noise
+
+            step_out = self.noise_scheduler.step(
+                pred_noises, t, intermidiate_noise
+            )
+
+            intermidiate_noise = step_out["prev_sample"]
+
+        print(f"denoised {intermidiate_noise}")
+
         assert 1==2
         
 
