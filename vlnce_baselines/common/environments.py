22394d8bd6fb2e4088b4a965e4019a9a620098ee
Yuhang Song <1595025647@qq.com>
2024-09-18 14:06:33 +0800

    trajectory prediction test

--- vlnce_baselines/common/environments.py
+++ vlnce_baselines/common/environments.py
@@ -37,11 +37,9 @@ class VLNCEDaggerEnv(habitat.RLEnv):
             agent_state.rotation.inverse(), np.array([0, 0, -1])
         )
         heading = cartesian_to_polar(-heading_vector[2], heading_vector[0])[1]
-        
-        print(heading.tolist())
-        
 
-        return agent_state.position.tolist().append(heading)
+
+        return agent_state.position.tolist().append(heading.tolist())
 
     
 
