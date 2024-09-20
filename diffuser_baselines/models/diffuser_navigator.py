0d90ccd390ef625bdb17a8f143e2477dc7e1dd73
Yuhang Song <1595025647@qq.com>
2024-09-20 14:53:54 +0800

    trajectory prediction test | testing

--- diffuser_baselines/models/diffuser_navigator.py
+++ diffuser_baselines/models/diffuser_navigator.py
@@ -291,8 +291,8 @@ class DiffusionNavigator(nn.Module):
         noise = torch.randn(observations["trajectories"][0].unsqueeze(0).shape, device=observations["trajectories"].device)
 
         noising_timesteps = torch.randint(
-            200,
-            300, # self.noise_scheduler.config.num_train_timesteps
+            0,
+            10, # self.noise_scheduler.config.num_train_timesteps
             (1,), device=noise.device
         ).long()
 
