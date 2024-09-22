fd1c1d33d30f19bb8a76729908a2750cac9826aa
Yuhang Song <1595025647@qq.com>
2024-09-22 12:54:45 +0800

    trajectory prediction norm

--- diffuser_baselines/models/diffuser_navigator.py
+++ diffuser_baselines/models/diffuser_navigator.py
@@ -339,6 +339,9 @@ class DiffusionNavigator(nn.Module):
         tokens = (tokens[0][0].unsqueeze(0),tokens[1][0].unsqueeze(0),tokens[2][0].unsqueeze(0),tokens[3][0].unsqueeze(0),tokens[4][0].unsqueeze(0),tokens[5][0].unsqueeze(0))
         pad_mask = pad_mask[0].unsqueeze(0)
 
+        for item in tokens:
+            print(item.shape)
+
         print(f"GroundTruth Trajectory {observations['trajectories'][0]}")
         
 
