888870f2bfdb7b9a669fee79175bbea61c066496
Yuhang <yuhang@topsoftint.com>
2024-09-30 13:09:12 +0800

    added histories exp. | testing

--- diffuser_baselines/models/diffuser_navigator.py
+++ diffuser_baselines/models/diffuser_navigator.py
@@ -268,7 +268,7 @@ class DiffusionNavigator(nn.Module):
         pose_feature = self.pose_encoder(observations["proprioceptions"]) 
 
         if inference: # dont pack
-            input_x = observations["rgb_features"] # take current as input and reshape to (bs,len,d)
+            input_x = observations["rgb_features"][:, :2048, :, :].view(observations["rgb_features"].shape[0],-1).unsqueeze(1) # take current as input and reshape to (bs,len,d)
             print(input_x.shape)
             history_tokens, next_hiddens = self.his_encoder(input_x,hiddens,inference=True)
         else:
