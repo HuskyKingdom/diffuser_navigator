5b274154b69b0c6ad62df0b33eac1e1459ff4fbe
Yuhang <yuhang@topsoftint.com>
2024-09-30 14:36:39 +0800

    refine context features exp.

--- diffuser_baselines/models/encoders/instruction_encoder.py
+++ diffuser_baselines/models/encoders/instruction_encoder.py
@@ -51,6 +51,11 @@ class InstructionEncoder(nn.Module):
         """
 
         input = observations.long()
+
+        print(self.embedding_layer(input).shape)
+        print(self.embedding_layer(input))
+        assert 1==2
+
         out = self.map_layer(self.embedding_layer(input))
         
         return out
