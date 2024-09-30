64ef529d87051ed53359a5e15dc9179efe1ccad2
Yuhang <yuhang@topsoftint.com>
2024-09-30 12:22:52 +0800

    added histories exp. | testing

--- diffuser_baselines/models/diffuser_navigator.py
+++ diffuser_baselines/models/diffuser_navigator.py
@@ -321,58 +321,55 @@ class DiffusionNavigator(nn.Module):
         loss = mse_loss
 
 
-        # # evaluations ____
+        # # # evaluations ____
 
-        loss = loss - loss # ignore update 
+        # loss = loss - loss # ignore update 
+        # noise = torch.randn(observations["trajectories"][0].unsqueeze(0).shape, device=observations["trajectories"].device)
 
-        noise = torch.randn(observations["trajectories"][0].unsqueeze(0).shape, device=observations["trajectories"].device)
+        # noising_timesteps = torch.randint(
+        #     999,
+        #     1000, # self.noise_scheduler.config.num_train_timesteps
+        #     (1,), device=noise.device
+        # ).long()
 
-        noising_timesteps = torch.randint(
-            999,
-            1000, # self.noise_scheduler.config.num_train_timesteps
-            (1,), device=noise.device
-        ).long()
-
-        noised_traj = self.noise_scheduler.add_noise(
-            observations["trajectories"][0].unsqueeze(0), noise,
-            noising_timesteps
-        )
+        # noised_traj = self.noise_scheduler.add_noise(
+        #     observations["trajectories"][0].unsqueeze(0), noise,
+        #     noising_timesteps
+        # )
         
 
-        denoise_steps = list(range(noising_timesteps[0].item(), -1, -1))
-
-        intermidiate_noise = noise
+        # denoise_steps = list(range(noising_timesteps[0].item(), -1, -1))
+        # intermidiate_noise = noise
     
-        for t in denoise_steps:
+        # for t in denoise_steps:
+        #     # noise pred.
+        #     with torch.no_grad():
 
-            # noise pred.
-            with torch.no_grad():
+        #         tokens = self.tokenlize_input(observations,intermidiate_noise)
+        #         tokens = (tokens[0][0].unsqueeze(0),tokens[1][0].unsqueeze(0),tokens[2][0].unsqueeze(0),tokens[3][0].unsqueeze(0),tokens[4][0].unsqueeze(0),tokens[5][0].unsqueeze(0))
+        #         pad_mask = pad_mask[0].unsqueeze(0)
 
-                tokens = self.tokenlize_input(observations,intermidiate_noise)
-                tokens = (tokens[0][0].unsqueeze(0),tokens[1][0].unsqueeze(0),tokens[2][0].unsqueeze(0),tokens[3][0].unsqueeze(0),tokens[4][0].unsqueeze(0),tokens[5][0].unsqueeze(0))
-                pad_mask = pad_mask[0].unsqueeze(0)
+        #         pred_noises = self.predict_noise(tokens,t * torch.ones(len(tokens[0])).to(tokens[0].device).long(),pad_mask)
 
-                pred_noises = self.predict_noise(tokens,t * torch.ones(len(tokens[0])).to(tokens[0].device).long(),pad_mask)
-
-            step_out = self.noise_scheduler.step(
-                pred_noises, t, intermidiate_noise
-            )
-
-
-            intermidiate_noise = step_out["prev_sample"]
-
-        denormed_groundtruth = self.denormalize_dim(observations['trajectories'][0])
-        denormed_pred = self.denormalize_dim(intermidiate_noise)
-
-        print(f"GroundTruth Trajectory {denormed_groundtruth} | ground truth actions {observations['gt_actions'][0]}")
-        print(f"Predicted Actions {denormed_pred}")
+        #     step_out = self.noise_scheduler.step(
+        #         pred_noises, t, intermidiate_noise
+        #     )
 
 
-        # analyzing
-        if self.total_evaled < 100:
-            self.total_evaled += 3
-        else:
-            assert 1==2
+        #     intermidiate_noise = step_out["prev_sample"]
+
+        # denormed_groundtruth = self.denormalize_dim(observations['trajectories'][0])
+        # denormed_pred = self.denormalize_dim(intermidiate_noise)
+
+        # print(f"GroundTruth Trajectory {denormed_groundtruth} | ground truth actions {observations['gt_actions'][0]}")
+        # print(f"Predicted Actions {denormed_pred}")
+
+
+        # # analyzing
+        # if self.total_evaled < 100:
+        #     self.total_evaled += 3
+        # else:
+        #     assert 1==2
 
 
 
