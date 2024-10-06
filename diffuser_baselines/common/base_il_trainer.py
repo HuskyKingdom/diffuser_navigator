21af918c4cc47bc4f3a5e926ecd5ac8198a4f8d5
Yuhang <yuhang@topsoftint.com>
2024-10-06 12:32:06 +0800

    improved complexity and bs | evaluation

--- diffuser_baselines/common/base_il_trainer.py
+++ diffuser_baselines/common/base_il_trainer.py
@@ -323,9 +323,9 @@ class BaseVLNCETrainer(BaseILTrainer):
                     with torch.no_grad():
                         out,hiddens = self.policy.act(batch,all_pose,hiddens,print_info=True) # print prediction info
                         action_candidates = out.cpu().tolist()
-                # else: # forward ONLY
-                #     with torch.no_grad():
-                #         _, hiddens = self.policy.act(batch,all_pose,hiddens)
+                else: # forward ONLY
+                    with torch.no_grad():
+                        _, hiddens = self.policy.act(batch,all_pose,hiddens)
                 
                 # pop actions & update hidden
                 actions = [[env_index.pop(0)] for env_index in action_candidates]
