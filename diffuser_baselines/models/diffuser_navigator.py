436e8b21839e03155f6776ed57b967fdba0dbc0f
Yuhang <yuhang@topsoftint.com>
2024-10-08 13:09:54 +0800

    improved history | testing

--- diffuser_baselines/models/diffuser_navigator.py
+++ diffuser_baselines/models/diffuser_navigator.py
@@ -41,7 +41,7 @@ class DiffusionPolicy(Policy):
         
 
         print(rgb_features.squeeze(0)[:2048, :, :].shape)
-        print(torch.stack(self.histories, dim=0).shape)
+        print(torch.stack(self.histories, dim=0).unsqueeze(0).shape)
         assert 1==2
 
 
