d89f6836af30c7b1f507003a595baf8cf6da6e27
Yuhang <yuhang@topsoftint.com>
2024-10-08 13:59:34 +0800

    improved history | testing

--- diffuser_baselines/models/common/multihead_custom_attention.py
+++ diffuser_baselines/models/common/multihead_custom_attention.py
@@ -352,7 +352,6 @@ def multi_head_attention_forward(query,  # type: Tensor
         assert bias_v is None
     
     print(rotary_pe)
-    assert 1==2
 
     if rotary_pe is not None:  # rotary pe ROPE disentangeld
         qp, kvp = rotary_pe
