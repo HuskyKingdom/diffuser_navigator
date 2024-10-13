7a4c5870f47a3e2eac8e7db0acc32838139838ec
Yuhang <yuhang@topsoftint.com>
2024-10-13 17:58:11 +0800

    back to GRU exp. | attention vis

--- diffuser_baselines/models/common/layers.py
+++ diffuser_baselines/models/common/layers.py
@@ -415,6 +415,7 @@ class FFWRelativeCrossAttentionModule(nn.Module):
         return output
 
 def vis_attention(weights, pad_mask, k=None):
+    
     import matplotlib.pyplot as plt
     import seaborn as sns
     import numpy as np
@@ -482,7 +483,7 @@ class FFWRelativeSelfAttentionModule(nn.Module):
             ))
 
     def forward(self, query, diff_ts=None,
-                query_pos=None, context=None, context_pos=None,pad_mask=None):
+                query_pos=None, context=None, context_pos=None,pad_mask=None,text=False):
         output = []
         for i in range(self.num_layers):
             query, attn_output_weights = self.attn_layers[i](
@@ -490,7 +491,8 @@ class FFWRelativeSelfAttentionModule(nn.Module):
             )
             query = self.ffw_layers[i](query, diff_ts)
             output.append(query)
-            vis_attention(attn_output_weights,pad_mask)
+            if text:
+                vis_attention(attn_output_weights,pad_mask)
         return output
 
 
