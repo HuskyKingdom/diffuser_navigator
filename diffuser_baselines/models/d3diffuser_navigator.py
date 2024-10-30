0a26ccdbf88235c9c7a6b148fc6447cb35d65b22
Yuhang <yuhang@topsoftint.com>
2024-10-30 17:53:41 +0800

    d3pm

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -155,18 +155,18 @@ class D3DiffusionNavigator(nn.Module):
             nn.Linear(embedding_dim, embedding_dim)
         )
 
-        self.pose_encoder = nn.Sequential(
-            SinusoidalPosEmb(embedding_dim),
-            nn.Linear(embedding_dim, embedding_dim),
-            nn.ReLU(),
-            nn.Linear(embedding_dim, embedding_dim)
-        )
-
         # self.pose_encoder = nn.Sequential(
-        #     nn.Linear(1, embedding_dim),
+        #     SinusoidalPosEmb(embedding_dim),
+        #     nn.Linear(embedding_dim, embedding_dim),
         #     nn.ReLU(),
         #     nn.Linear(embedding_dim, embedding_dim)
         # )
+
+        self.pose_encoder = nn.Sequential(
+            nn.Linear(1, embedding_dim),
+            nn.ReLU(),
+            nn.Linear(embedding_dim, embedding_dim)
+        )
 
 
         self.time_emb = nn.Sequential(
