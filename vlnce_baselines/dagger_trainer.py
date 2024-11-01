f064452640882f5d25d047d9995a4873eb239461
Yuhang <yuhang@topsoftint.com>
2024-11-01 14:02:29 +0800

    d3pm | eval

--- vlnce_baselines/dagger_trainer.py
+++ vlnce_baselines/dagger_trainer.py
@@ -100,7 +100,7 @@ def collate_fn(batch):
     )
     not_done_masks[0] = 0
 
-    
+    print(corrected_actions_batch.shape)
     assert 1==2
 
     observations_batch = ObservationsDict(observations_batch)
