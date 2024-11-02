5602cecdf4a76238d4014a525381a023297b60b6
Yuhang <yuhang@topsoftint.com>
2024-11-02 16:51:27 +0800

    full traj

--- diffuser_baselines/d3diffuser_trainer.py
+++ diffuser_baselines/d3diffuser_trainer.py
@@ -93,6 +93,8 @@ def collate_fn(batch):
     data_dicts = new_data_dicts
 
 
+    print(data_dict['instruction'])
+
     # Determine the maximum sequence length in the batch
     max_seq_len = max(len(ele) for ele in gt_actions_batch)
 
@@ -101,9 +103,9 @@ def collate_fn(batch):
     for data_dict, prev_actions, gt_actions, trajectories in zip(data_dicts, prev_actions_batch, gt_actions_batch, trajectories_batch):
 
         # Pad and collect 'instruction'
-        collected_data['instruction'].append(_pad_helper(torch.tensor(data_dict['instruction']), max_seq_len))
+        collected_data['instruction'].append(_pad_helper(data_dict['instruction'], max_seq_len))
         
-        print(data_dict['rgb_features'])
+        
         # Pad and collect 'rgb_features'
         collected_data['rgb_features'].append(_pad_helper(data_dict['rgb_features'], max_seq_len))
         
