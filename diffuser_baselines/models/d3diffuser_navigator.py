246b7fe40a6b4ea425e27efc704abd0d45bf68c8
Yuhang <yuhang@topsoftint.com>
2024-11-03 17:28:37 +0800

    full traj

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -183,7 +183,7 @@ class D3DiffusionNavigator(nn.Module):
 
         # Other Encoders
         self.instruction_encoder = InstructionEncoder(config,embedding_dim)
-        self.action_encoder = nn.Embedding(num_actions, embedding_dim)
+        self.action_encoder = nn.Embedding(num_actions, embedding_dim/2)
         
 
 
