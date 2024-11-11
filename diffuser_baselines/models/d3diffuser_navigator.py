6d04ae6534c162d47b461ea527072a807a4cae6c
Yuhang <yuhang@topsoftint.com>
2024-11-11 18:35:12 +0800

    full traj

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -294,7 +294,8 @@ class D3DiffusionNavigator(nn.Module):
             action_features = self.action_encoder(action_input.long()) # (B+T,) -> (B+T, emb)
 
         else: # compute action featrues based on decoder outputs (prev actions)
-
+            
+            action_except = observations['prev_actions'][:, 1:] # remove first
             action_start_token = torch.full((observations['prev_actions'].shape[0], 1), 4).to(observations['prev_actions'].device) # add start token
             action_input = torch.cat([action_start_token, observations['prev_actions']], dim=1) # construct input
             print(action_input)
