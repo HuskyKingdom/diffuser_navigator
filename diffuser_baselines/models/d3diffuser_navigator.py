e40ce8ec5a36c0e713655afcb9303e176c0f200d
Yuhang <yuhang@topsoftint.com>
2024-11-11 18:43:25 +0800

    full traj

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -75,7 +75,8 @@ class D3DiffusionPolicy(Policy):
 
         
         action = self.navigator(collected_data,(B,T), inference = True)
-        self.pre_actions.append(prev_actions.item())
+        if prev_actions.item() != 0:
+            self.pre_actions.append(prev_actions.item())
 
         if print_info:
             print(f"Action inferenced : {action.item()}, with history length {len(self.pre_actions)}")
