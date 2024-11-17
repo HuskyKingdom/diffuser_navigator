670f975ba314dd54522f9e5d581fec46c67781ca
Yuhang <yuhang@topsoftint.com>
2024-11-17 13:30:24 +0800

    full traj

--- diffuser_baselines/models/encoders/instruction_encoder.py
+++ diffuser_baselines/models/encoders/instruction_encoder.py
@@ -59,7 +59,7 @@ class InstructionEncoder(nn.Module):
         out = self.pe_layer(out)
 
         out,_ = self.language_self_atten(out.transpose(0,1), diff_ts=None,
-                query_pos=None, context=None, context_pos=None,pad_mask=pad_mask,vis=True,ins_text=ins_text)
+                query_pos=None, context=None, context_pos=None,pad_mask=pad_mask,vis=False,ins_text=ins_text)
         out = out[-1].transpose(0,1)
         
         return out
