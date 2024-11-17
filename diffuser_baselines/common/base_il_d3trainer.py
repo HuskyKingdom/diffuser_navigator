ebafdf5bbc43ddd1d0c37ecfc4365792d9bd2fb6
Yuhang <yuhang@topsoftint.com>
2024-11-17 16:55:52 +0800

    full traj

--- diffuser_baselines/common/base_il_d3trainer.py
+++ diffuser_baselines/common/base_il_d3trainer.py
@@ -566,7 +566,7 @@ class BaseVLNCETrainer(BaseILTrainer):
 
             # print info for anylyze
             ins_text = current_episodes[i].instruction.instruction_text
-            print(ins_text)
+            # print(ins_text)
 
             actions = self.policy.act(batch,prev_actions,print_info=True,ins_text=ins_text)
             prev_actions.copy_(actions)
