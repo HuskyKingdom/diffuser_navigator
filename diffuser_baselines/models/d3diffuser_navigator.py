ae49b7665b361eef1b7278916374e7581cb97a11
Yuhang <yuhang@topsoftint.com>
2024-11-25 16:36:41 +0800

    eval

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -303,9 +303,14 @@ class D3DiffusionNavigator(nn.Module):
         else: # compute action featrues based on decoder outputs (prev actions)
             
             print( observations['prev_actions'], observations['prev_actions'].shape)
-            assert 1==2
+            
             action_start_token = torch.full((observations['prev_actions'].shape[0], 1), 4).to(observations['prev_actions'].device) # add start token
+            
             action_input = torch.cat([action_start_token, observations['prev_actions']], dim=1) # construct input
+
+            print( action_input, action_input.shape)
+            assert 1==2
+
             action_input = action_input.view(-1,) # # (B,T) -> (B+T,)
             action_features = self.action_encoder(action_input.long()) # (B+T,) -> (B+T, emb)
 
