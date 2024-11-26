0646e43f4c2340b74a872165570771884f75e8f9
Yuhang <yuhang@topsoftint.com>
2024-11-26 15:10:45 +0800

    regular pos enc

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -337,13 +337,12 @@ class D3DiffusionNavigator(nn.Module):
 
 
         print(decoder_pred)
-        print(observations["gt_actions"].long()[1,5:])
-        print(observations["gt_actions"].long().shape)
+        print(observations["gt_actions"].long()[1,:10])
         assert 1==2
 
         decoder_pred = self.decoder(context_feature,observations["padding_mask"], enc_out, encoder_pad_mask, causal_mask)
-        print(decoder_pred[1,5:,:])
-        print(observations["gt_actions"].long()[1,5:])
+        print(decoder_pred[1,:10,:])
+        print(observations["gt_actions"].long()[1,:10])
         assert 1==2
 
         loss = self.masked_CE(decoder_pred,observations["gt_actions"].long(), observations["lengths"],  observations["weights"]).sum()
