5fdc1af1ae81eab48bc2c27cca14c62c946fd4bc
Yuhang <yuhang@topsoftint.com>
2024-11-26 17:04:27 +0800

    regular pos enc

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -96,7 +96,7 @@ class D3DiffusionPolicy(Policy):
         
         
         print(observations['rgb_features'].shape)
-        print(observations['rgb_features'][1,0,:])
+        print(observations['rgb_features'][1,0,:],observations['rgb_features'][1,0,:].shape)
         assert 1==2
 
 
