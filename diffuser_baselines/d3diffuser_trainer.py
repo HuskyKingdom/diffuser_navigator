62119128593b97dc0014af15063f9fed3deac8ff
Yuhang <yuhang@topsoftint.com>
2024-11-29 13:21:07 +0800

    consistancy fixed

--- diffuser_baselines/d3diffuser_trainer.py
+++ diffuser_baselines/d3diffuser_trainer.py
@@ -483,7 +483,11 @@ class D3DiffuserTrainer(BaseVLNCETrainer):
                     )
                     if envs.num_envs == 0:
                         break
-                
+
+                if collected_eps == 1:
+                    print(current_episodes[i].instruction.instruction_text)
+                    assert 1==2
+
                 if (torch.rand_like(prev_actions.long(), dtype=torch.float) < beta):
                     actions = self.policy.act(
                     batch,prev_actions,encode_only = True
