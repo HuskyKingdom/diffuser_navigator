3e903bc01b3d6957a4362fd81d8009211fff5403
Yuhang <yuhang@topsoftint.com>
2024-12-02 14:04:43 +0800

    more epochs

--- diffuser_baselines/models/encoders/trajectory_decoder.py
+++ diffuser_baselines/models/encoders/trajectory_decoder.py
@@ -59,7 +59,7 @@ class TrajectoryDecoder(nn.Module):
                 q_pad_mask=dec_pad_mask,
                 k_pad_mask=enc_pad_mask,
                 causal_mask=causal_mask,
-                vis=True,
+                vis=False,
                 ins_text=ins_text)
         
         
