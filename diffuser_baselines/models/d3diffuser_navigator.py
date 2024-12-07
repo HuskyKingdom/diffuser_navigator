ad32c0f42822923fa16115faecb5424d5bf0a6eb
Yuhang <yuhang@topsoftint.com>
2024-12-07 17:34:49 +0800

    dagger

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -248,7 +248,7 @@ class D3DiffusionNavigator(nn.Module):
             # construct input as [<start>,...]
             # action_except = observations['gt_actions'][:, :-1] # remove last element <end>
             action_except = observations['prev_actions'][:, 1:] # remove first
-            action_start_token = torch.full((observations['prev_actions'].shape[0], 1), 4).to(observations['prev_actions'].device) # add start token
+            action_start_token = torch.full((observations['prev_actions'].shape[0], 1), 4).to(observations['prev_actions'].device).long() # add start token
             action_input = torch.cat([action_start_token, action_except], dim=1) # construct input
             action_input = action_input.view(-1,) # # (B,T) -> (B+T,)
             action_features = self.action_encoder(action_input.long()) # (B+T,) -> (B+T, emb)
@@ -256,8 +256,7 @@ class D3DiffusionNavigator(nn.Module):
         else: # compute action featrues based on decoder outputs (prev actions)
             
             
-            action_start_token = torch.full((observations['prev_actions'].shape[0], 1), 4).to(observations['prev_actions'].device) # add start token
-            print(action_start_token,observations['prev_actions'])
+            action_start_token = torch.full((observations['prev_actions'].shape[0], 1), 4).to(observations['prev_actions'].device).long() # add start token
             action_input = torch.cat([action_start_token, observations['prev_actions']], dim=1) # construct input
 
             
