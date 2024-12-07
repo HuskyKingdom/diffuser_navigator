290cbef4e03ade8a5e0acb3fa3fa3f12549955aa
Yuhang <yuhang@topsoftint.com>
2024-12-07 15:27:05 +0800

    dagger

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -349,8 +349,8 @@ class D3DiffusionNavigator(nn.Module):
 
         decoder_pred = self.decoder(context_feature,observations["padding_mask"], enc_out, encoder_pad_mask, causal_mask)
 
-        print(decoder_pred[10,:,:])
-        print(observations["gt_actions"].long()[10,:])
+        print(decoder_pred[20,:,:])
+        print(observations["gt_actions"].long()[20,:])
         assert 1==2
 
         loss = self.masked_CE(decoder_pred,observations["gt_actions"].long(), observations["lengths"],  observations["weights"]).sum()
