cc0ee9c8611b42276c16dcbe3716d632d5733296
Hang <1595025647@qq.com>
2024-12-08 00:27:31 +0800

    Update d3diffuser_navigator.py

--- diffuser_baselines/models/d3diffuser_navigator.py
+++ diffuser_baselines/models/d3diffuser_navigator.py
@@ -249,7 +249,7 @@ class D3DiffusionNavigator(nn.Module):
             # action_except = observations['gt_actions'][:, :-1] # remove last element <end>
             action_except = observations['prev_actions'][:, 1:] # remove first
             action_start_token = torch.full((observations['prev_actions'].shape[0], 1), 4).to(observations['prev_actions'].device).long() # add start token
-            action_input = torch.cat([action_start_token, action_except], dim=1) # construct input
+            action_input = torch.cat([action_start_token, action_except.long()], dim=1) # construct input
             action_input = action_input.view(-1,) # # (B,T) -> (B+T,)
             action_features = self.action_encoder(action_input.long()) # (B+T,) -> (B+T, emb)
 
