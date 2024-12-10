a74729a4d0280a4a838e612434371e5a4186dadd
Yuhang <yuhang@topsoftint.com>
2024-12-10 16:54:49 +0800

    dagger_ddp

--- diffuser_baselines/__init__.py
+++ diffuser_baselines/__init__.py
@@ -1,7 +1,8 @@
 from diffuser_baselines import (
     diffuser_trainer,
     ddp_diffuser_trainer,
-    d3diffuser_trainer
+    d3diffuser_trainer,
+    d3ddp_trainer
 )
 from vlnce_baselines.common import environments
 from diffuser_baselines.models import diffuser_navigator,d3diffuser_navigator
