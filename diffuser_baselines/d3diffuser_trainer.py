f39e3c5320208ae383e63250961c96b4f81a39c7
Yuhang <yuhang@topsoftint.com>
2024-12-15 18:05:05 +0800

    pg_monitor & aug

--- diffuser_baselines/d3diffuser_trainer.py
+++ diffuser_baselines/d3diffuser_trainer.py
@@ -144,7 +144,7 @@ def collate_fn(batch):
             continue
         collected_data[key] = torch.stack(collected_data[key], dim=0)
 
-    print(collected_data["progress"].append(pad_progress).shape)
+    print(collected_data["progress"].shape)
     assert 1==2
 
     return collected_data
