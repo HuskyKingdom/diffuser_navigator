fa3b764d9757374326d678d3585c28ad04b4ae90
Yuhang <yuhang@topsoftint.com>
2024-12-16 15:03:54 +0800

    debug

--- diffuser_baselines/models/encoders/resnet_encoders.py
+++ diffuser_baselines/models/encoders/resnet_encoders.py
@@ -216,8 +216,7 @@ class TorchVisionResNet(nn.Module):
                 .view(1, -1, h, w)
                 .expand(b, self.spatial_embeddings.embedding_dim, h, w)
             )
-            print(torch.cat([resnet_output, spatial_features], dim=1),torch.cat([resnet_output, spatial_features], dim=1).shape)
-            assert 1==2
+            
             return torch.cat([resnet_output, spatial_features], dim=1)
         else:
             return self.fc(resnet_output)  # returns [BATCH x OUTPUT_DIM]
