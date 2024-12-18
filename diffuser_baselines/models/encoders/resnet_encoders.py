30307240e2a9bff955a738077f0fa3de796ae117
Yuhang <yuhang@topsoftint.com>
2024-12-18 16:16:03 +0800

    eval on rgb observations

--- diffuser_baselines/models/encoders/resnet_encoders.py
+++ diffuser_baselines/models/encoders/resnet_encoders.py
@@ -202,7 +202,7 @@ class TorchVisionResNet(nn.Module):
             # # show frame
             import cv2
             import matplotlib.pyplot as plt
-            print(rgb_observations.unsqueeze(0).cpu().numpy().shape)
+            print(rgb_observations.squeeze(0).cpu().numpy().shape)
             image_rgb = cv2.cvtColor(rgb_observations.unsqueeze(0).cpu().numpy(), cv2.COLOR_BGR2RGB)
             # 显示图片
             plt.imshow(image_rgb)
