eea1929674a7de3c19e7b5938fb7f5e25e99e474
Yuhang <yuhang@topsoftint.com>
2024-12-22 18:39:33 +0800

    bs fixed

--- diffuser_baselines/d3ddp_trainer.py
+++ diffuser_baselines/d3ddp_trainer.py
@@ -801,7 +801,7 @@ class D3DiffuserTrainer(BaseVLNCETrainer):
                 ddp_sampler = DistributedSampler(diffusion_dataset)
                 diter = torch.utils.data.DataLoader(
                     diffusion_dataset,
-                    batch_size=self.config.IL.batch_size // self.world_size,
+                    batch_size=self.config.IL.batch_size,
                     shuffle=False,
                     sampler=ddp_sampler,
                     collate_fn=collate_fn,
